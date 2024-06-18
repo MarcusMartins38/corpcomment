9c4ba1ffe2f1b15a0f8e948b48937e355c247f32
unknown <marcusmartins38@gmail.com>
2024-06-17 21:26:52 -0300

    chore: setup initial style project body

---
+++ src/components/Footer.tsx
@@ -0,0 +1,19 @@
+export default function Footer() {
+  return (
+    <footer>
+      <small>
+        <p>
+          &copy; Copyright by{" "}
+          <a target="_blank" href="https://github.com/MarcusMartins38">
+            Marcus
+          </a>
+          . Intended for learning or your portfolio.
+        </p>
+        <p>
+          <span className="u-bold u-italic">Not allowed</span> to use as your
+          own material.
+        </p>
+      </small>
+    </footer>
+  );
+}
