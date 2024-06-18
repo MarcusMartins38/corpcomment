25bcc6f712f440436488618db74c8212fda4efc0
unknown <marcusmartins38@gmail.com>
2024-06-17 22:44:59 -0300

    refactor: start to use await async

--- src/components/FeedbackList.tsx
+++ src/components/FeedbackList.tsx
@@ -1,26 +1,43 @@
 import { useEffect, useState } from "react";
 import FeedbackItem from "./FeedbackItem";
 import Spinner from "./Spinner";
+import ErrorMessage from "./ErrorMessage";
 
 export default function FeedbackList() {
   const [feedbackItems, setFeedbackItems] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const fetchFeedbackItems = async () => {
+    setIsLoading(true);
+
+    try {
+      const response = await fetch(
+        "https://bytegrad.com/course-assets/projects/corpcomment/api/feedbacks"
+      );
+
+      if (!response.ok) {
+        throw new Error();
+      }
+
+      const data = await response.json();
+      setFeedbackItems(data.feedbacks);
+    } catch (error) {
+      setErrorMessage("Something went wrong, try again later.");
+    }
+
+    setIsLoading(false);
+  };
 
   useEffect(() => {
-    setIsLoading(true);
-    fetch(
-      "https://bytegrad.com/course-assets/projects/corpcomment/api/feedbacks"
-    )
-      .then((res) => res.json())
-      .then((data) => {
-        setFeedbackItems(data.feedbacks);
-        setIsLoading(false);
-      });
+    fetchFeedbackItems();
   }, []);
 
   return (
     <ol className="feedback-list">
-      {isLoading ? <Spinner /> : null}
+      {isLoading && <Spinner />}
+
+      {errorMessage && <ErrorMessage message={errorMessage} />}
 
       {feedbackItems.map((feedbackItem) => (
         <FeedbackItem key={feedbackItem.id} feedbackItem={feedbackItem} />
